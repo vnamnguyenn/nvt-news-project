a63d700678d6280aed30451998abb97cc4f2742c
vnamnguyenn <vnamnguyenn@gmail.com>
2022-03-28 20:03:15 +0700

    Create side bar

--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -1,31 +1,57 @@
 import React from 'react';
 import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import FeedIcon from '@mui/icons-material/Feed';
+import VideoLibraryIcon from '@mui/icons-material/VideoLibrary';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import CategoryIcon from '@mui/icons-material/Category';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
 			<div className="sidebar__top">
 				<span className="sidebar__logo">NVT Blog</span>
 			</div>
-      <hr />
+			<hr />
 			<div className="sidebar__center">
 				<ul>
+					<p className="title">MAIN</p>
 					<li>
+						<DashboardIcon className="icon" />
 						<span>Dashboard</span>
 					</li>
+					<p className="title">List</p>
 					<li>
+						<PersonOutlineIcon className="icon" />
+						<span>Accounts</span>
+					</li>
+					<li>
+						<FeedIcon className="icon" />
 						<span>Posts</span>
 					</li>
 					<li>
+						<VideoLibraryIcon className="icon" />
 						<span>Videos</span>
 					</li>
 					<li>
+						<CategoryIcon className="icon" />
 						<span>Categories</span>
 					</li>
 					<li>
+						<LocalOfferIcon className="icon" />
 						<span>Tags</span>
 					</li>
+					<p className="title">Users</p>
 					<li>
-						<span>Accounts</span>
+						<AccountCircleOutlinedIcon className="icon" />
+						<span>Profile</span>
+					</li>
+          <li>
+            <ExitToAppIcon className="icon" />
+						<span>Logout</span>
 					</li>
 				</ul>
 			</div>
