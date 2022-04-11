b16945fe0bbef889cb2b3cc95f2df89b21328dc7
vnamnguyenn <vnamnguyenn@gmail.com>
2022-04-12 04:52:04 +0700

    commit

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,7 +1,7 @@
 const AuthController = require('../modules/auth/authController');
 module.exports = async (app) => {
-    app.post('/signin', AuthController.signin);
-    app.post('/signup', AuthController.signup);
+    app.post('/api/signin', AuthController.signin);
+    app.post('/api/signup', AuthController.signup);
     // app.post('/api/update_password', AuthController.updatePassword);
     // app.post('/api/update_profile', AuthController.updateProfile);
 };
