4c00010a39221b3dcf85758aa1d7868599db568f
vnamnguyenn <vnamnguyenn@gmail.com>
2022-04-12 03:56:42 +0700

    git commit

--- api/routes/users.js
+++ api/routes/users.js
@@ -5,9 +5,9 @@ const {
 	verifyTokenAndAdmin,
 } = require('../middleware/verifyToken');
 module.exports = async (app) => {
-	app.get('/user/:id', verifyTokenAndAuthorization, UserController.findByID);
-	app.get('/user/', UserController.getAll);
-	app.post('/user/create/', UserController.create);
-	app.patch('/user/edit/:id', UserController.update);
-	app.delete('/user/delete/:id', UserController.deleteByID);
+	app.get('/api/user/:id', verifyTokenAndAuthorization, UserController.findByID);
+	app.get('/api/user/', UserController.getAll);
+	app.post('/api/user/create/', UserController.create);
+	app.patch('/api/user/edit/:id', UserController.update);
+	app.delete('/api/user/delete/:id', UserController.deleteByID);
 };
--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:9000/";
+const BASE_URL = "http://localhost:9000/api";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
