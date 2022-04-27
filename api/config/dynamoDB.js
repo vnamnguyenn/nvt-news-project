c479fea9d2e42801e76670305c85388c0b6b695d
vnamnguyenn <74644568+vnamnguyenn@users.noreply.github.com>
2022-04-27 20:32:18 +0700

    Update dymamodb.js

--- api/config/dynamoDB.js
+++ api/config/dynamoDB.js
@@ -3,7 +3,7 @@ AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: process.env.AWS_DEFAULT_REGION,
-  endpoint: process.env.AWS_END_POINT_DDB_CONSOLE,
+  endpoint: process.env.AWS_END_POINT_DDB_LOCAL,
 });
 
 const db = new AWS.DynamoDB.DocumentClient({convertEmptyValues: true});
